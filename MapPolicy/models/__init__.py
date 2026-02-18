ef0a58ce240b1f6f195f57e640cfcf043c857b42
Zehao-Du <duzehao_2023jd@sjtu.edu.cn>
2026-02-18 08:34:38 +0000

    bnmlp version

--- MapPolicy/models/__init__.py
+++ MapPolicy/models/__init__.py
@@ -1,3 +1,4 @@
 from MapPolicy.models.actor import \
     DemoModel, SingleFrame_MLP, SingleFrame_MLP_Chamferloss, \
-    SingleFrame_GNN, SingleFrame_Lift3d_GNN, SingleFrame_Lift3d_PointNet_GNN
+    SingleFrame_GNN, SingleFrame_Lift3d_GNN, SingleFrame_Lift3d_PointNet_GNN, \
+    SingleFrame_Lift3d_PointNet_GNN_BNMLP
